d89962ee61d370ae0507689f861704a65ffbbf11
Seungyun <rats96@snu.ac.kr>
2026-02-05 14:31:08 +0900

    v1.1

--- main.py
+++ main.py
@@ -169,8 +169,6 @@ def main():
     config['solver']['scheduler']['params']['warmup_lr'] = warmup_lr * (args.lr / base_lr)
 
     print(f'lr with {args.lr}')
-
-    os.makedirs(args.save_dir, exist_ok=True)
     
     logger = Logger(args)
     logger.save_config(config)
